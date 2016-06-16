76bbed56f5afb3e76961274e1f5c63a42ddfa861
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-16 22:03:02 +0100

    ram

--- ram.vhd
+++ ram.vhd
@@ -12,3 +12,17 @@ entity ram is
 			q:       out std_logic_vector(31 downto 0));
 end ram;
 
+architecture a of ram is
+		type mem is array(0 to 31) of std_logic_vector(31 downto 0);
+		signal storage: mem;
+begin
+  	process(clock)
+	begin
+		if (clock'event and clock = '1') then
+			if(we = '1') then
+					storage(wr_addr) <= data;
+			end if;
+			q <= storage(rd_addr);
+		end if;
+	end process;
+end a;
